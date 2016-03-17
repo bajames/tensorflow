739b0856dc980b71f121ff1e8b5a904fbd188d3b
Barnaby James <bajames@gmail.com>
2016-03-17 07:47:36 -0700

    Script for running tensorflow on docker

---
+++ run_tensorflow.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+eval $(docker-machine env default)
+docker-machine ip default
+#docker cp ~/ml/notMNIST_large.tar.gz compassionate_ramanujan:/notebooks
+#docker cp ~/ml/notMNIST_small.tar.gz compassionate_ramanujan:/notebooks
+docker run -p 8888:8888 -v /Users/bjames/ml/tensorflow/tensorflow/examples/udacity -it b.gcr.io/tensorflow/tensorflow
+#docker run -p 8888:8888 -v /Users/bjames/ml/tensorflow/tensorflow/examples/udacity -it --rm $USER/assignments
